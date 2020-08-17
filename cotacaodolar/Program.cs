b24cdb234498a88bab51b558b88faaf730b9a34d
Delbo.Bruno <delbosilvaster@gmail.com>
2020-08-17 14:27:56 -0300

    first commit

---
+++ cotacaodolar/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using HtmlAgilityPack;
+
+namespace cotacaodolar {
+    class Program {
+        static void Main(string[] args) {
+
+            var doc = new HtmlWeb().Load("https://valor.globo.com/valor-data/");
+
+            var cotDolares = doc.DocumentNode.SelectNodes("//main/div/div/div/div/div/div");
+            int total = 1;
+
+            string CaminhoNome = "cotdolar.txt";
+            StreamWriter x = File.CreateText(CaminhoNome);
+
+            foreach (var cotDolar in cotDolares) {
+                if (total == 1) {
+                    x.WriteLine(cotDolar.InnerText);
+                }
+                else {
+                    break;
+                }
+                total = total + 1;
+            }
+
+            x.Close();
+
+        }
+    }
+}
